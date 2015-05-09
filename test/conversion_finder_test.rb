de16b0df1e8e6dcc3593820749ace395c92fd240
Brian Thomas Storti <btstorti@gmail.com>
2015-05-09 00:29:58 -0300

    add more tests for conversion_finder

--- test/conversion_finder_test.rb
+++ test/conversion_finder_test.rb
@@ -3,10 +3,30 @@ require_relative '../lib/conversion_finder'
 require_relative '../lib/search_params'
 
 class ConversionFinderTest < Minitest::Test
-  def test_finds_conversion_value
-    finder = ConversionFinder.new("test/assets/test.csv")
-    search_params = SearchParams.new({"metric_id" => "15", "date" => "2014-07-05"})
+  def test_finds_conversion_value_for_specific_date
+    search_params = SearchParams.new({"metric_id" => "15",
+                                      "date" => "2014-07-05"})
 
-    assert_equal 9, finder.find(search_params)
+    assert_equal 1, finder.find(search_params)
+  end
+
+  def test_finds_conversion_value_for_week
+    search_params = SearchParams.new({"metric_id" => "30",
+                                      "date" => "2014-07-07",
+                                      "end_date" => "2014-07-13"})
+
+    assert_equal 4, finder.find(search_params)
+  end
+
+  def test_finds_conversion_value_for_month
+    search_params = SearchParams.new({"metric_id" => "15",
+                                      "date" => "2014-07"})
+
+    assert_equal 5, finder.find(search_params)
+  end
+
+  private
+  def finder
+    ConversionFinder.new("test/assets/test.csv")
   end
 end
