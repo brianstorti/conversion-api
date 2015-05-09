80e9b36c71fe375a7501de28b1dc24466e8bc50f
Brian Thomas Storti <btstorti@gmail.com>
2015-05-08 23:20:44 -0300

    SearchParams converts parameters to the right format

--- lib/search_params.rb
+++ lib/search_params.rb
@@ -1,6 +1,17 @@
+require 'date'
+
 class SearchParams
+  attr_reader :start_date, :end_date, :time_range
+
   def initialize(params)
     @metric_id = params["metric_id"]
+    @start_date = process_start_date(params["date"])
+    @end_date = process_end_date(params["end_date"])
+    @time_range = find_time_range(params["date"], params["end_date"])
+  end
+
+  def metric_id
+    @metric_id.to_i
   end
 
   def validate!
@@ -11,5 +22,34 @@ class SearchParams
     end
 
     errors
+  end
+
+  private
+  def process_start_date(date)
+    return unless date
+    year, month, day = date.split("-").map(&:to_i)
+    day ||= 1
+
+    john_time_format(year, month, day)
+  end
+
+  def process_end_date(end_date)
+    return unless end_date
+
+    year, month, day = end_date.split("-").map(&:to_i)
+    john_time_format(year, month, day)
+  end
+
+  def find_time_range(date, end_date)
+    return unless date
+
+    return 7 if end_date
+    return 30 if date.split("-").size == 2
+    return 1
+  end
+
+  def john_time_format(year, month, day)
+    date = Date.new(year, month, day)
+    (date - Date.new(2009, 01, 01)).to_i
   end
 end
--- test/search_params_test.rb
+++ test/search_params_test.rb
@@ -8,4 +8,15 @@ class SearchParamsTest < Minitest::Test
 
     assert_equal [{parameter: "metric_id", error: "required parameter"}], errors
   end
+
+  def test_creates_parameters
+    params = SearchParams.new({ "metric_id" => "1",
+                                "date" => "2014-03-01",
+                                "end_date" => "2014-03-8" })
+
+    assert_equal 1, params.metric_id
+    assert_equal 1885, params.start_date
+    assert_equal 1892, params.end_date
+    assert_equal 7, params.time_range
+  end
 end
