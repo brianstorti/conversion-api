dccdfe983916bf8591e4dffe2e57dec6fe6e15d9
Brian Thomas Storti <btstorti@gmail.com>
2015-05-10 15:07:49 -0300

    refactor SearchParams

--- lib/search_params.rb
+++ lib/search_params.rb
@@ -1,13 +1,26 @@
 require "date"
 
 class SearchParams
-  attr_reader :metric_id, :start_date, :end_date, :time_range
+  attr_reader :metric_id, :params
+  private :params
 
   def initialize(params)
     @metric_id = params["metric_id"]
-    @start_date = convert_date(params["date"]) || convert_month(params["month"])
-    @end_date = convert_date(params["end_date"])
-    @time_range = find_time_range(params["end_date"], params["month"])
+    @params = params
+  end
+
+  def start_date
+    convert_date(params["date"]) || convert_month(params["month"])
+  end
+
+  def end_date
+    convert_date(params["end_date"])
+  end
+
+  def time_range
+    return 7 if params["end_date"]
+    return 1 if params["date"]
+    return 30
   end
 
   private
@@ -16,12 +29,6 @@ class SearchParams
 
     date = Date.parse(month, "YY-mm-dd")
     convert_date(date)
-  end
-
-  def find_time_range(end_date, month)
-    return 30 if month
-    return 7 if end_date
-    return 1
   end
 
   # Converts a date to the JohnTime format.
--- test/search_params_test.rb
+++ test/search_params_test.rb
@@ -4,22 +4,25 @@ require "minitest/autorun"
 require_relative "../lib/search_params"
 
 class SearchParamsTest < Minitest::Test
-  def test_creates_parameters
-    params = SearchParams.new({ "metric_id" => 1,
-                                "date" => Date.new(2014, 3, 1),
-                                "end_date" => Date.new(2014, 3, 8) })
-
+  def test_converts_date
+    params = SearchParams.new({ "date" => Date.new(2014, 3, 1) })
     start_date_john_time = 1885
+
+    assert_equal start_date_john_time, params.start_date
+  end
+
+  def test_converts_end_date
+    params = SearchParams.new({ "end_date" => Date.new(2014, 3, 8) })
     end_date_john_time = 1892
 
-    assert_equal 1, params.metric_id
-    assert_equal start_date_john_time, params.start_date
     assert_equal end_date_john_time, params.end_date
-    assert_equal 7, params.time_range
   end
 
   def test_knows_time_range_for_day
     params = SearchParams.new({ "date" => Date.new })
+    assert_equal 1, params.time_range
+
+    params = SearchParams.new({ "date" => Date.new, "month" => "foo" })
     assert_equal 1, params.time_range
   end
 
