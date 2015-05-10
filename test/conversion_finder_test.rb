126da29658f531adbe2f77502d80ef03000fbd09
Brian Thomas Storti <btstorti@gmail.com>
2015-05-10 18:11:23 -0300

    remove redundant curly braces

--- test/conversion_finder_test.rb
+++ test/conversion_finder_test.rb
@@ -11,23 +11,23 @@ class ConversionFinderTest < Minitest::Test
   end
 
   def test_finds_conversion_value_for_specific_date
-    search_params = SearchParams.new({ "metric_id" => 15,
-                                       "date" => Date.new(2014, 7, 5) })
+    search_params = SearchParams.new("metric_id" => 15,
+                                     "date" => Date.new(2014, 7, 5))
 
     assert_equal 1, finder.find(search_params)
   end
 
   def test_finds_conversion_value_date_range
-    search_params = SearchParams.new({"metric_id" => 30,
-                                      "date" => Date.new(2014, 7, 7),
-                                      "end_date" => Date.new(2014, 7, 13)})
+    search_params = SearchParams.new("metric_id" => 30,
+                                     "date" => Date.new(2014, 7, 7),
+                                     "end_date" => Date.new(2014, 7, 13))
 
     assert_equal 4, finder.find(search_params)
   end
 
   def test_finds_conversion_value_for_month
-    search_params = SearchParams.new({"metric_id" => 15,
-                                      "month" => "2014-07-01"})
+    search_params = SearchParams.new("metric_id" => 15,
+                                     "month" => "2014-07-01")
 
     assert_equal 5, finder.find(search_params)
   end
--- test/search_params_test.rb
+++ test/search_params_test.rb
@@ -6,34 +6,34 @@ require_relative "../lib/search_params"
 
 class SearchParamsTest < Minitest::Test
   def test_converts_date
-    params = SearchParams.new({ "date" => Date.new(2014, 3, 1) })
+    params = SearchParams.new("date" => Date.new(2014, 3, 1))
     start_date_in_john_time = 1885
 
     assert_equal start_date_in_john_time, params.start_date
   end
 
   def test_converts_end_date
-    params = SearchParams.new({ "end_date" => Date.new(2014, 3, 8) })
+    params = SearchParams.new("end_date" => Date.new(2014, 3, 8))
     end_date_in_john_time = 1892
 
     assert_equal end_date_in_john_time, params.end_date
   end
 
   def test_knows_time_range_for_day
-    params = SearchParams.new({ "date" => Date.new })
+    params = SearchParams.new("date" => Date.new)
     assert_equal 1, params.time_range
 
-    params = SearchParams.new({ "date" => Date.new, "month" => "foo" })
+    params = SearchParams.new("date" => Date.new, "month" => "foo")
     assert_equal 1, params.time_range
   end
 
   def test_knows_time_range_for_week
-    params = SearchParams.new({ "date" => Date.new, "end_date" => Date.new })
+    params = SearchParams.new("date" => Date.new, "end_date" => Date.new)
     assert_equal 7, params.time_range
   end
 
   def test_knows_time_range_for_month
-    params = SearchParams.new({ "month" => "2011-1-1" })
+    params = SearchParams.new("month" => "2011-1-1")
     assert_equal 30, params.time_range
   end
 end
